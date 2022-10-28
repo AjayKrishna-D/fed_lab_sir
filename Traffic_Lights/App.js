4cecb0fdcb64670128fcbeffc408fc983baaeca1
AjayKrishna-D <96071706+AjayKrishna-D@users.noreply.github.com>
2022-10-28 22:37:10 +0530

    Create App.js

---
+++ Traffic_Lights/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+import ChangeColors from './components/ChangeColors.js/ChangeColors';
+function App() {
+ return (
+ <div style={{width:'90%', margin: '0 auto'}}>
+ <ChangeColors />
+ </div>
+ ); }
+export default App;
